90a2ed42ef2a74b2e492a69d497186be40d38ef3
jihyun-j <jihyun9142@gmail.com>
2025-10-01 14:00:13 +0900

    (feat) Workout 페이지에 CircleButton으로 시작 버튼 변경

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,19 +1,19 @@
+import CircleButton from "@/components/ui/CircleButton";
 import * as Location from "expo-location";
-import React, { useEffect, useRef, useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from "react-native-maps";
+import { useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+// * Workout Page: 사용자의 현재위치. 러닝 시작 버튼. 내 근처 공유된 러닝 루트.
 
 export default function Workout() {
   const [currentLocation, setCurrentLocation] =
     useState<Location.LocationObject | null>(null);
-  const [isRunning, setIsRunning] = useState(false);
-  const [route, setRoute] = useState<{ latitude: number; longitude: number }[]>(
-    []
-  );
+
   const [errorMeg, setErrorMeg] = useState<string | null>(null);
 
-  const watchRef = useRef<Location.LocationSubscription | null>(null);
-  const mapRef = useRef<MapView | null>(null);
+  const router = useRouter();
 
   // 사용자의 현재 위치 초기에 가져오기
   useEffect(() => {
@@ -30,71 +30,12 @@ export default function Workout() {
     }
 
     getCurrentLocation();
-
-    // 언마운트 시 구독 정리
-    return () => {
-      watchRef.current?.remove();
-      watchRef.current = null;
-    };
   }, []);
-
-  const startTracking = async () => {
-    if (watchRef.current) return;
-
-    const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      setErrorMeg("위치 접근 권한이 필요합니다.");
-      return;
-    }
-
-    watchRef.current = await Location.watchPositionAsync(
-      {
-        accuracy: Location.Accuracy.BestForNavigation,
-        timeInterval: 1000, // 1초마다 업데이트
-        distanceInterval: 3, // 3m 이동 시 업데이트
-        mayShowUserSettingsDialog: true, //
-      },
-      (location) => {
-        const { latitude, longitude } = location.coords;
-
-        // 경로 좌표 누적
-        setRoute((prev) => [...prev, { latitude, longitude }]);
-
-        // 현재 위치 갱신
-        setCurrentLocation(location);
-
-        // 지도 카메라 따라다니기
-        mapRef.current?.animateCamera({
-          center: { latitude, longitude },
-          zoom: 17,
-        });
-      }
-    );
-
-    setIsRunning(true);
-  };
-
-  // 러닝 일시정지
-  const pauseTracking = async () => {
-    await watchRef.current?.remove();
-    watchRef.current = null;
-    setIsRunning(false);
-  };
-
-  // 러닝 시작/일시정지 토글
-  const toggleTracking = () => {
-    if (isRunning) {
-      pauseTracking();
-    } else {
-      startTracking();
-    }
-  };
 
   return (
     <View style={styles.container}>
       {currentLocation && (
         <MapView
-          ref={mapRef}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
             latitude: currentLocation?.coords.latitude,
@@ -105,20 +46,19 @@ export default function Workout() {
           showsUserLocation
           followsUserLocation={true}
           style={styles.map}>
-          {route.length > 1 && (
-            <Polyline coordinates={route} strokeWidth={6} geodesic />
-          )}
-
-          {route.length > 0 && <Marker coordinate={route[0]} title="Start" />}
+          <Marker coordinate={currentLocation?.coords} />
         </MapView>
       )}
-      <Pressable onPress={toggleTracking} style={styles.startBtn}>
-        <Text>{isRunning ? "PAUSE" : "START"}</Text>
-      </Pressable>
-
-      <Pressable onPress={toggleTracking} style={styles.startBtn}>
-        <Text>{isRunning ? "PAUSE" : "START"}</Text>
-      </Pressable>
+      <View style={styles.startBtn}>
+        <CircleButton
+          onPress={() => router.push("/start")}
+          width={100}
+          height={100}
+          backgroundColor="#085FDE"
+          textColor="white"
+          text="START"
+        />
+      </View>
     </View>
   );
 }
@@ -134,11 +74,17 @@ const styles = StyleSheet.create({
   startBtn: {
     position: "absolute",
     bottom: 50,
-    left: 150,
-    paddingVertical: 30,
-    paddingHorizontal: 30,
-    borderRadius: 100,
-    backgroundColor: "#2563EB",
-    elevation: 4,
+    left: "50%",
+    transform: [
+      {
+        translateX: -50,
+      },
+    ],
   },
 });
+
+// 1. 지도에서 사용자의 현재 위치를 보여준다
+// 2. 현재 위치에서 사용자가 '시작'을 누르면 러닝을 시작한다. 시간/칼로리/거리/페이스를 UI를 보여준다.
+// 3. 러닝이 시작되면 출발점을 저장하고 표시한다. 그리고 시간/칼로리/거리/페이스를 기록한다.
+// 4. 러닝 중 일시정지를 하면 시간/거리/페이스/칼로리 기록을 정지한다. 러닝을 다시 시작 여부를 확인하는 알림을 띄운다.
+// 5. 사용자가 '정지'를 누르면 러닝 기록을 저장하고, 피드 공유 여부를 물어본다.
