bab5547f18a9a302e4dbc6a6da306582fe167efa
jihyun-j <jihyun9142@gmail.com>
2025-10-02 22:52:47 +0900

    (feat) RunProvider 컴포넌트 업데이트: 위치 추적 기능 추가 및 거리 계산 로직 구현

--- providers/RunProvider.tsx
+++ providers/RunProvider.tsx
@@ -1,10 +1,15 @@
+import { calcDistance } from "@/util/util";
+import * as Location from "expo-location";
 import React, { createContext, useContext, useRef, useState } from "react";
 
-type RunStatus = "running" | "paused" | "stopped";
+export type RunStatus = "running" | "paused" | "stopped";
+export type LatLon = { latitude: number; longitude: number };
 
-type RunContextType = {
+export type RunContextType = {
   status: RunStatus;
   seconds: number;
+  path: LatLon[];
+  distanceKm: number;
   startRunning: () => void;
   pauseRunning: () => void;
   stopRunning: () => void;
@@ -20,8 +25,14 @@ export default function RunProvider({
 }) {
   const [status, setStatus] = useState<RunStatus>("stopped");
   const [seconds, setSeconds] = useState(0);
-  const timeRef = useRef<number | null>(null);
+  const [path, setPath] = useState<LatLon[]>([]);
+  const [distanceKm, setDistanceKm] = useState(0);
 
+  const timeRef = useRef<number | null>(null);
+  const watchSubRef = useRef<Location.LocationSubscription>(null);
+  const lastPointRef = useRef<LatLon | null>(null);
+
+  // 타이머
   const startTimer = () => {
     if (!timeRef.current) {
       timeRef.current = setInterval(() => {
@@ -37,28 +48,84 @@ export default function RunProvider({
     }
   };
 
-  const startRunning = () => {
+  // 위치 구독
+  const startTracking = async () => {
+    const { status: permission } =
+      await Location.requestForegroundPermissionsAsync();
+    if (permission !== "granted") throw new Error("위치 권한이 필요합니다.");
+
+    watchSubRef.current = await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 0,
+      },
+      (location) => {
+        const position: LatLon = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        };
+
+        setPath((prev) => {
+          if (prev.length === 0) {
+            lastPointRef.current = position;
+            return [position];
+          }
+          const last = lastPointRef.current ?? prev[prev.length - 1];
+
+          const disKm = calcDistance(last, position);
+
+          if (disKm > 0.0005) {
+            setDistanceKm((km) => km + disKm);
+            lastPointRef.current = position;
+            return [...prev, position];
+          }
+          return prev;
+        });
+      }
+    );
+  };
+
+  const stopWatching = () => {
+    watchSubRef.current?.remove();
+    watchSubRef.current = null;
+  };
+
+  // 러닝 시작
+  const startRunning = async () => {
+    setStatus("running");
+    setSeconds(0);
+    setPath([]);
+    setDistanceKm(0);
+    lastPointRef.current = null;
+    startTimer();
+    await startTracking();
+  };
+
+  // 러닝 일시 정지
+  const pauseRunning = () => {
+    if (status !== "running") return;
+    setStatus("paused");
+    clearTimer();
+    stopWatching();
+  };
+
+  // 러닝 일시 정지 후 다시 시작
+  const resumeRunning = async () => {
+    if (status !== "paused") return;
     setStatus("running");
     startTimer();
+    await startTracking();
   };
 
-  const pauseRunning = () => {
-    if (status === "running") {
-      setStatus("paused");
-      clearTimer();
-    }
-  };
-
-  const resumeRunning = () => {
-    if (status === "paused") {
-      setStatus("running");
-      startTimer();
-    }
-  };
+  // 러닝 종료
   const stopRunning = () => {
     setStatus("stopped");
     clearTimer();
     setSeconds(0);
+    stopWatching();
+    setPath([]);
+    setDistanceKm(0);
   };
 
   return (
@@ -66,6 +133,8 @@ export default function RunProvider({
       value={{
         status,
         seconds,
+        path,
+        distanceKm,
         startRunning,
         stopRunning,
         pauseRunning,
