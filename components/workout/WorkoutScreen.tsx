8bf6c948520513fb39f88177e1a61f61cf1ce74a
jihyun-j <jihyun9142@gmail.com>
2025-10-02 21:33:53 +0900

    (update) WorkoutScreen 컴포넌트 수정: BottomSheet의 snapPoints 조정 및 상태에 따른 StartButton 및 RecordModal 렌더링 로직 변경

--- components/workout/WorkoutScreen.tsx
+++ components/workout/WorkoutScreen.tsx
@@ -1,16 +1,18 @@
 import StartButton from "@/components/workout/StartButton";
 import { useRun } from "@/providers/RunProvider";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
-import React, { useMemo, useRef } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ModalBackground from "./ModalBackground";
 import RecordModal from "./RecordModal";
 import WorkoutMap from "./WorkoutMap";
 
 export default function WorkoutScreen() {
   // 모달 사이즈
-  const snapPoints = useMemo(() => ["3%", "30%", "100%"], []);
+  const snapPoints = useMemo(() => ["35%", "95%"], []);
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const [snapPointIndex, setSnapPointIndex] = useState(0);
 
   // 러닝 상태
   const { status, startRunning } = useRun();
@@ -20,12 +22,17 @@ export default function WorkoutScreen() {
       {/* 지도 */}
       <WorkoutMap />
       {/* 시작버튼 */}
-      {status === "idle" && <StartButton onPress={startRunning} />}
+      {status === "stopped" && <StartButton onPress={startRunning} />}
       {/* 모달 */}
-      {status !== "idle" && (
-        <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints} index={1}>
+      {status !== "stopped" && (
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={snapPoints}
+          index={0}
+          onChange={setSnapPointIndex}
+          backgroundComponent={ModalBackground}>
           <BottomSheetView style={styles.modalContainer}>
-            <RecordModal />
+            <RecordModal snapPointIndex={snapPointIndex} />
           </BottomSheetView>
         </BottomSheet>
       )}
@@ -38,7 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   modalContainer: {
-    flex: 1,
-    alignItems: "center",
+    height: "100%",
   },
 });
